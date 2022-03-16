9c13b3d629427de359ae39dc4402828920d83374
DFjay <pezhikov@gmail.com>
2022-03-16 21:31:48 +0500

    Поправил ConstructorInjection.java

--- src/main/java/com/example/demo/ConstructorInjection.java
+++ src/main/java/com/example/demo/ConstructorInjection.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
@@ -8,7 +7,6 @@ import org.springframework.stereotype.Component;
 public class ConstructorInjection {
     private IAnimal animal;
 
-    @Autowired
     public ConstructorInjection(@Qualifier("dog") IAnimal animal) {
         this.animal = animal;
     }
